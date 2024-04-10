be43d436627eda5c87d86f0d41e8a2d3643bf859
David Friar <david.friar@cognifide.com>
2024-04-10 21:44:03 +0100

    try again to make it compile

--- app/include/zmk/usb_hid.h
+++ app/include/zmk/usb_hid.h
@@ -14,3 +14,7 @@ int zmk_usb_hid_send_consumer_report(void);
 int zmk_usb_hid_send_mouse_report(void);
 #endif // IS_ENABLED(CONFIG_ZMK_MOUSE)
 void zmk_usb_hid_set_protocol(uint8_t protocol);
+
+// FIXME: adding this to make the plover hid branch compile. This may not be the right way to do
+// this
+int zmk_usb_hid_send_report(const uint8_t *report, size_t len);
